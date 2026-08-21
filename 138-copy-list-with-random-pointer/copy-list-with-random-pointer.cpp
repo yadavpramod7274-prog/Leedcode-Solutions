572f7608e793acb5bd3b4196c72dcd3990b10c90
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-08-22 00:31:24 +0530

    Time: 11 ms (14.92%) | Memory: 15.1 MB (48.53%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,64 @@
+// class Node {
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        //  step 1->create the deep copy without random pointer
+         if(head==NULL) return NULL;
+         Node* dummy = new Node(0);
+         Node* tempC=dummy;
+          Node* temp=head;
+          while(temp){
+            Node* a=new Node(temp->val);
+             tempC->next=a;
+              tempC= tempC->next;
+             temp=temp->next;
+          }
+          Node* duplicate = dummy->next;       
+             // step 2 alternate connection
+              Node* a=head;
+              Node* b=duplicate;
+              dummy = new Node(-1);
+               Node* tempD= dummy;
+              while(a){
+             tempD->next=a;
+              a=a->next;
+               tempD= tempD->next;
+               tempD->next=b;
+              b=b->next;
+               tempD= tempD->next;
+              }
+             dummy= dummy->next;
+                // step 3 alternate connection
+              Node* t1=dummy;// t1 will traverse in the original list;
+              while(t1 ){
+                Node* t2=t1->next; // t2 is for duplicate;
+                  if(t1->random) t2->random=t1->random->next;
+                  if(t1->next)  t1 = t1->next->next;
+                  
+              }
+              // step 4 -> removing the connection 
+               Node* d1=new Node(-1);
+                Node* d2=new Node(-1);
+                 t1=d1;
+                 Node* t2=d2;
+                  Node* t=dummy;
+                   while(t){
+                   t1->next=t;
+                     t=t->next;
+                    t1=t1->next;
+                   
+
+                     t2->next=t;
+                       t=t->next;
+                   t2=t2->next;
+                   
+                   }
+                   t1->next=NULL;
+                    t2->next=NULL;
+                    d1=d1->next;// origin with random
+                    d2=d2->next;
+                     return d2;
+     }
+};
