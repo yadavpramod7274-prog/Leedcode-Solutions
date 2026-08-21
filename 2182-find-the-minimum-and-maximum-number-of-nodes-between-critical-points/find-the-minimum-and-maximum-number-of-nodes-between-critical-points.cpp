68c357970c4ecbbe2b053eff0417b873e3c6e606
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-08-21 18:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 124.6 MB (82.28%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -10,40 +10,30 @@ public:
         ListNode*b=head->next;
         ListNode*c=head->next->next;
         if(c==NULL) return {-1,-1};
-
+         int mind=INT_MAX;
+          int f=1;
+           int s=-1;
         while(c){
             if( b->val > a->val && b->val > c->val || b->val < c->val && b->val < a->val ){
-           if(fidx==-1) fidx=idx;
+           if(fidx==-1) fidx=idx;// max
             else sidx=idx;
+            
+            // min
+            f=s;
+            s=idx;
+             if(f!=-1){
+            int d= s-f;
+              mind=min (mind,d);
             }
-            a=a->next;
-            b=b->next;
-            c=c->next;
-             idx++;
         }
-        if(sidx==-1) return {-1,-1};
-        int maxid = sidx-fidx;
-           int mind=INT_MAX;
-            idx=1;
-           fidx=-1;
-          sidx=-1;
-           a=head;
-           b=head->next;
-           c=head->next->next;
-
-        while(c){
-            if( b->val > a->val && b->val > c->val || b->val < c->val && b->val < a->val ){
-          fidx=sidx;
-           sidx=idx;
-           if(fidx!=-1){
-            int d= sidx-fidx;
-              mind=min(mind,d);}
-            }
+       
             a=a->next;
             b=b->next;
             c=c->next;
              idx++; 
         }
+        if(sidx==-1) return {-1,-1};
+         int maxid= sidx-fidx;
         return {mind,maxid};
     }
 };
