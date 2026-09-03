30556e81b608ad31ebab0ce0fe4799d7c4d4359d
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-09-03 23:00:41 +0530

    Time: 2 ms (85.03%) | Memory: 161.3 MB (80.78%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -8,12 +8,35 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * }; 
  */
+ListNode* reverseList( ListNode*head){
+   ListNode* curr=NULL;
+   while(head){
+     ListNode* nextNode=head->next;
+      head->next=curr;
+       curr=head;
+       head=nextNode;
+   }
+   return curr;
+}
+
 class Solution {
 public:
     ListNode* removeNodes(ListNode* head) {
-       if(head==NULL) return NULL;
-       head->next=removeNodes(head->next);
-       return head->next && head->val<head->next->val ? head->next: head;
-         
+    
+   //ListNode* removeNodes(ListNode* head){
+        head=reverseList(head);
+        int maxval=head->val;
+        ListNode* curr=head;
+        while(curr->next){
+            if(curr->next->val < maxval){
+                curr->next= curr->next->next;
+            }
+            else{
+                curr=curr->next;
+                maxval=curr->val;
+            }
+        }
+           head=reverseList(head);
+        return head;
     }
 };
