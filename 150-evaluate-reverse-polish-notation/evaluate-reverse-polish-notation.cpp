03885fa072f0eb1e2d14c26c329b86c9e5f93507
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-09-06 19:22:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (67.29%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int evalRPN(vector<string>& t) {
-        stack<int>st;
+         stack<int>st;
         int n=t.size();
-        for(int i=0;i<n;i++){
+         for(int i=0;i<n;i++){
             string s=t[i];
-            if(s=="+" || s=="-" ||s=="*"||s=="/" ){
-               int a=st.top(); st.pop(); // pahali valuve bharne ke liye
-                int b=st.top(); st.pop();// dusri value;
+            if(s=="+" || s=="-" || s=="*" || s=="/"){
+                int a =st.top(); st.pop(); //pahale value
+                 int  b=st.top(); st.pop();//
 
-                if(s=="+" ) st.push(b+a);
-                 if(s=="-" ) st.push(b-a);
-                 if(s=="*" ) st.push(b*a);
-                 if(s=="/" ) st.push(b/a);
-                
+                 if(s=="+" ) st.push(b+a);
+                    if(s=="-" ) st.push(b-a);
+                    if(s=="*" ) st.push(b*a); 
+                     if(s=="/" ) st.push(b/a);
+
             }
-
-            else {
+            else{
                 st.push(stoi(s));
             }
-        }
-        return st.top();
-        
+         }
+         return st.top();
     }
 };
