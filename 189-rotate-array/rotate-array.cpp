dcd14a8674458b3576794eecc24f1d94c3384eae
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-09-13 16:04:43 +0530

    Time: 3 ms (37.99%) | Memory: 263.4 MB (33.31%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+ void reverse(vector<int>& nums,int p,int q){
+    while(p<=q){
+        int temp= nums[p];
+         nums[p]=nums[q];
+          nums[q]=temp;
+          p++;
+           q--;
+
+    }
+  }
+
+    void rotate(vector<int>& nums, int k) {
+      int n=nums.size();
+      if(k>n){
+       k= k%n;
+      }
+     reverse(nums,0,n-1);
+       reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+    }
+
+};
