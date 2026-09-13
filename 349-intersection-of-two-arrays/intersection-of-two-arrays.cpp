f322606eabf12ce9896b22d33ab9e772a5817433
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-09-13 17:28:51 +0530

    Time: 11 ms (5.59%) | Memory: 13.8 MB (97.96%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+       sort(nums1.begin(),nums1.end()); 
+
+        sort(nums2.begin(),nums2.end());   
+         vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+
+                if(nums1[i]==nums2[j]){
+                 if(ans.size()==0 || ans.back()!=nums1[i] ){
+                  ans.push_back(nums1[i]);
+          }
+                }
+            //      else if (nums1[i] > nums2[j]) {
+            //       j++;
+            // }
+            // else {
+            //      i++;
+            // }
+            }
+        }  
+        return ans;
+    }
+};
