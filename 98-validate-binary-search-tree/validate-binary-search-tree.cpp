7da61c902135ddf8eabc8786a629fa81d4655557
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-09-16 21:01:06 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (15.96%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/validate-binary-search-tree.cpp
@@ -1,21 +1,22 @@
 
 class Solution {
 public:
-void  Inorder(TreeNode* root, vector<int> &ans){
+   TreeNode*  prev = NULL;
+     bool flag= true;
+void  Inorder(TreeNode* root){
     if(root==NULL) return;  // base case
-     Inorder(root->left,ans); // call1
-      ans.push_back(root->val);// work
-     Inorder(root->right,ans); // call2
-
+     Inorder(root->left); // call1
+      if(prev!=NULL){
+        if(root->val <= prev->val){
+             flag = false;
+        }
+      }
+       prev=root;
+     Inorder(root->right); // call2
+      
   }
     bool isValidBST(TreeNode* root) {
-     
-      vector<int>ans;
-     
-      Inorder(root,ans);
-       for(int i=1;i<ans.size();i++){
-        if(ans[i]<=ans[i-1])return false;
-       }
-       return true;
+      Inorder(root);
+       return flag;
     }
 };
