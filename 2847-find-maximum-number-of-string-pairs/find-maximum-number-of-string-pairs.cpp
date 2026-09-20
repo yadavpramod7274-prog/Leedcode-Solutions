76e98433cbd81fd999e5baa89127c68cf321b41c
yadavpramod7274-prog <yadavpramod7274@gmail.com>
2026-09-20 16:36:18 +0530

    Time: 8 ms (7.41%) | Memory: 25.5 MB (60.65%) - LeetSync

--- 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/find-maximum-number-of-string-pairs.cpp
@@ -4,17 +4,13 @@ public:
        int n= arr.size();
        int count=0;
        unordered_set<string> s; 
-      for(int i=0;i<n-1;i++){
-        s.insert(arr[i]);
-      }
          for(int j=0;j<n;j++){
            string rev =arr[j]; 
            reverse(rev.begin(),rev.end());
            if(arr[j]==rev) continue;
-           if(s.find(rev)!=s.end()){
-            count++;
-            s.erase(arr[j]);
-         }
+           if(s.find(rev)!=s.end()) count++;
+            else s.insert(arr[j]);
+         
       }
       return count; 
     }
